2fbb8994a57295ec071555eca9b17013646016a5
ABF00711 <abfjustice00711@gmail.com>
2025-07-21 13:54:03 -0600

    feat(views): add login_view, logout_view view

--- PythonPOC/myapp/views.py
+++ PythonPOC/myapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username = username, password = password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'mysite/login.html', {'error': 'Invalid credentials'})
+    return render(request, 'mysite/login.html')
+
+@login_required
+def home(request):
+    return render(request, 'mysite/home.html', {'user':request.user})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
